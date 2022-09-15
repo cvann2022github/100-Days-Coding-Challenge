42469e82e824ad3c9d05316f2e34b0ca329dcb1e
ANN VINCENT CHITTILAPPILLY <113539974+cvann2022github@users.noreply.github.com>
2022-09-15 23:27:11 +0530

    code1.cpp

---
+++ code1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+        int num = 99;
+        if(num%2==0)
+        cout<<num<<" is even. ";
+        else
+        cout<<num<<" is odd. ";
+        return 0;
+}
+  
