7485199ee31ace5f967f4db38fb8a24a0425cacf
ANN VINCENT CHITTILAPPILLY <113539974+cvann2022github@users.noreply.github.com>
2022-09-18 01:06:31 +0530

    code2.cpp

--- code2.cpp
+++ code2.cpp
@@ -1,15 +1,18 @@
+
 #include<iostream>
 using namespace std;
  int main()
   {
     int x,y,i;
-        cout<<"\t\t"<<"Print the Truth Table"<<"\n\n";
-    cout<<" X\tY\tX.Y\tX+Y\tX'\tY'\tX'.Y'\tX'+Y'\n";
+        cout<<"\t\t"<<"Print the De Morgan's Truth Table"<<"\n\n";
+    cout<<" X\tY\tX.Y\tX+Y\tX'\tY'\tX'.Y'\tX'+Y'\t((X+Y)')\n";
     for(i = 0;i <= 3;i++)
     {
         x = i / 2;
         y = i % 2;
-        cout<<" "<<x<<"\t"<<y<<"\t"<<(x&&y)<<"\t"<<(x||y)<<"\t"<<!x<<"\t"<<!y<<"\t"<<(!x&&!y)<<"\t"<<(!x||!y)<<"\t"<<endl;
+        cout<<" "<<x<<"\t"<<y<<"\t"<<(x&&y)<<"\t"<<(x||y)<<"\t"<<!x<<"\t"<<!y<<"\t"<<(!x&&!y)<<"\t"<<(!x||!y)<<"\t"<<(!(x||y))<<"\t";
+    
+        cout<<"Hence proved that ((A+B)') = (A'.B') and ((A.B)')=(A'+B')"<<endl;
     }
     return 0;
   }
