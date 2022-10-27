74dc78303d15e6dc409b012355631f68ae28f9c0
ANN VINCENT CHITTILAPPILLY <113539974+cvann2022github@users.noreply.github.com>
2022-10-28 00:02:57 +0530

    code39.cpp

---
+++ code39.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+void swap(int,int);
+int main()
+{
+        int a,b;
+        cout<<"Enter Value Of A :: ";
+        cin>>a;
+        cout<<"\nEnter Value of B :: ";
+        cin>>b;
+        cout<<"\nBefore Swapping, Value of :: \n\tA = "<<a<<"\tB = "<<b<<"\n";
+        swap(a,b);
+        cout<<"\nOutside Function After Swapping, Value of :: \n\tA = "<<a<<"\tB = "<<b<<"\n";
+}
+void swap(int a,int b)
+{
+        int c;
+        c=a;
+        a=b;
+        b=c;
+        cout<<"\nInside Function After Swapping, Value of :: \n\tA = "<<a<<"\tB = "<<b<<"\n";
+}
