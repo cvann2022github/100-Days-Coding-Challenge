ff99e220b3e914c718b19c9323a272a0f9b6097d
ANN VINCENT CHITTILAPPILLY <113539974+cvann2022github@users.noreply.github.com>
2022-10-29 21:20:44 +0530

    code42.java

---
+++ code42.java
@@ -0,0 +1,10 @@
+
+public class Example2 {
+    public static void main(String[] args) {
+        int num = 15;
+        for(int i = 1; i <= 10; ++i)
+        {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+    }
+}
