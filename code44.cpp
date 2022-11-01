b8b45c1b8625668c6a82f943e0c97514862c1bb2
ANN VINCENT CHITTILAPPILLY <113539974+cvann2022github@users.noreply.github.com>
2022-11-01 13:28:18 +0530

    code44.cpp

--- code44.cpp
+++ code44.cpp
@@ -3,19 +3,34 @@
 using namespace std;
 class Student
 {
+	private :
+		char name[30];
+		int rollNo;
+		int total;
+		float perc;
 	public:
-	int rollNo;
-	string stdName;
-	float perc;
+		void getDetails(void);
+		void putDetails(void);
 };
+void Student:: getDetails(void)
+{
+	cout<<"Enter name:";
+	cin>>name;
+	cout<<"Enter Roll number:";
+	cin>>rollNo;
+	cout<<"Enter total:";
+	cin>>total;
+	perc=(float)total/500*100;
+}
+void Student:: putDetails(void)
+{
+	cout<<"Student Details";	
+	cout<<"name<<name<<"roll number"<<rollNo<<"total"<<total<<"percentage"<<perc;
+}	
 int main()
 {
 	Student std;
-	std.rollNo=05;
-	std.stdName="Ann";
-	std.perc=98.20f;
-	cout<<"Student's RollNo.:"<<std.rollNo<<"\n";
-	cout<<"Student's Name:"<<std.stdName<<"\n";
-	cout<<"Student's percentage:"<<std.perc<<"\n";
+	std.getDetails();
+	std.putDetails();
 	return 0;
 }
