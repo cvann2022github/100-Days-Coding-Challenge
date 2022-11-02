a4e34a5e44f0b5f89ab8909fe32b7bd979e743be
ANN VINCENT CHITTILAPPILLY <113539974+cvann2022github@users.noreply.github.com>
2022-11-02 20:24:07 +0530

    code49.java

---
+++ code49.java
@@ -0,0 +1,11 @@
+
+public class Example4
+{
+    public static void main(String [] args)
+    {
+        int num1=97, num2=64,sum;
+        System.out.println("Enter two numbers:");
+        sum=num1+num2;
+        System.out.println("Sum is:"+sum);
+    }
+}
