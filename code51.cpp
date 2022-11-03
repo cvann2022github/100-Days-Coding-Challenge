b90581048b9fd31cf87f53d9c9152346afd0cfa1
ANN VINCENT CHITTILAPPILLY <113539974+cvann2022github@users.noreply.github.com>
2022-11-03 11:37:14 +0530

    code51.cpp

---
+++ code51.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+int fnum;
+int snum;
+int sum;
+cout<<"Enter two numbers:";
+cin>>fnum>>snum;
+sum=fnum+snum;
+cout<<"Sum is :"<<sum;
+return 0;
+}
+
