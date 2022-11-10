b4f91138aafae8b7ec008e9b7edfda083f5c1dc5
ANN VINCENT CHITTILAPPILLY <113539974+cvann2022github@users.noreply.github.com>
2022-11-10 18:57:15 +0530

    code58.cpp

---
+++ code58.cpp
@@ -0,0 +1,11 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+ string letters[2][4] = {
+ { "A", "B", "C", "D" },
+ { "E", "F", "G", "H" }
+};
+cout << letters[0][2]; // Outputs "C"
+}
