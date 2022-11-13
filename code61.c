c9cf9caaed81e387781237bc3cd38fd328442186
ANN VINCENT CHITTILAPPILLY <113539974+cvann2022github@users.noreply.github.com>
2022-11-13 21:02:25 +0530

    code61.c

---
+++ code61.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+int main()
+{
+int i=4;
+switch(i)
+{
+default:
+printf("This is default\n");case 1:
+printf("This is case 1\n");
+break;
+case 2:
+printf("This is case 2\n");break;
+case 3:
+printf("This is case 3\n");
+}
+return 0;
+}
