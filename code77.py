8cd4e7c7d07453eea1bb01f92bfe7672ad098b1c
ANN VINCENT CHITTILAPPILLY <113539974+cvann2022github@users.noreply.github.com>
2022-12-10 23:43:24 +0530

    code77.py

---
+++ code77.py
@@ -0,0 +1,13 @@
+
+name="Ambili"
+age=45
+print(name)
+print(age)
+
+first_name = "shradha"
+last_name = "khapra"
+age = 12
+is_adult = True
+print(first_name + " " + last_name)
+print(age)
+print(is_adult)
