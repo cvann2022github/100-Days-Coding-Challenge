bd3db071577e4658aeebfe87bc6eddc2657889d0
ANN VINCENT CHITTILAPPILLY <113539974+cvann2022github@users.noreply.github.com>
2022-12-11 00:04:27 +0530

    code78.py

---
+++ code78.py
@@ -0,0 +1,16 @@
+
+name = "Tony Stark"
+print(name.upper())
+print(name)
+print(name.lower())
+print(name)
+print(name.find('y'))
+print(name.find('Y'))
+print(name.find("Stark"))
+print(name.find("stark"))
+print(name.replace("Tony Stark", "Ironman"))
+print(name)
+#to check if a character/string is part of the main string
+print("Stark" in name)
+print("S" in name)
+print("s" in name)
