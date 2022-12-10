33ee409dba053101c51100fe9a488aab2735bdb8
ANN VINCENT CHITTILAPPILLY <113539974+cvann2022github@users.noreply.github.com>
2022-12-11 00:12:04 +0530

    code79.py

---
+++ code79.py
@@ -0,0 +1,16 @@
+
+name = input("What is your name?")
+question = input("What is your superhero name?")
+print("Hello" + name + question)
+
+old_age = input("Enter your age : ")
+new_age = int(old_age) + 2
+print(new_age)
+
+number=19
+print(float(number))
+
+first_number = input("Enter 1st number : ")
+second_number = input("Enter 2nd number : ")
+sum = float(first_number) + float(second_number)
+print("the sum is : " + str(sum))
